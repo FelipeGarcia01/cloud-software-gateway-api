47d2e4d4539a07dd2ce83b0455b7665a103effd4
felipe <lf.garciar1@uniandes.edu.co>
2023-04-16 17:15:20 -0500

    integration files

--- routes/file.py
+++ routes/file.py
@@ -1,6 +1,8 @@
 import json
+from io import BytesIO
+
 import requests
-from flask import Blueprint, Response, request
+from flask import Blueprint, Response, request, send_file
 from flask_jwt_extended import jwt_required
 from config import APIPaths
 
@@ -10,13 +12,16 @@ file_routes = Blueprint('file', __name__)
 @file_routes.route('/<int:id_file>', methods=['GET'])
 @jwt_required()
 def get_tasks(id_file: int):
-    uri = "{}{}{}".format(APIPaths.TASK_HOST, APIPaths.TASK_SINGLE_QUERY_PATH, id_file)
-    response: Response = requests.get(uri, headers={"Authorization": "{}".format(request.headers.get('Authorization'))})
+    type = request.args.get('type', None)
+    if type is not None:
+        uri = "{}{}{}?type={}".format(APIPaths.FILE_HOST, APIPaths.FILE_QUERY_PATH, id_file, type)
+        response: Response = requests.get(uri, headers={"Authorization": "{}".format(request.headers.get('Authorization'))})
+        if response.status_code != 200:
+            return {
+                "code": "fail",
+                "msg": "failed during search file, please contact with support"
+            }, response.status_code
+        filename = response.headers['Content-Disposition'].split("=")[1]
+        return send_file(BytesIO(response.content), download_name=filename)
 
-    response_body = json.loads(response.text)
-    if response.status_code != 200:
-        return {
-            "code": "fail",
-        }, response.status_code
-
-    return {}
+    return {"fail": "type required"}, 400
