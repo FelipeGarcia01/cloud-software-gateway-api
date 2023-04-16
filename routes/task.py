0ca3a2c2ee42662c747dc5bbec38030dd8f93aba
Luis <luisfernandosantaospina@gmail.com>
2023-04-16 10:44:34 -0500

    Add response

--- routes/task.py
+++ routes/task.py
@@ -1,8 +1,6 @@
-import json
-
 import requests
-from flask import Blueprint, Response, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask import Blueprint, Response, request, json
+from flask_jwt_extended import jwt_required
 from config import APIPaths
 
 task_routes = Blueprint('tasks', __name__)
@@ -11,16 +9,12 @@ task_routes = Blueprint('tasks', __name__)
 @task_routes.route('/', methods=['GET'])
 @jwt_required()
 def get_tasks():
-
     max = request.args.get('max', None)
     order = request.args.get('order', 0)
 
-    uri ="{}{}?order={}".format(APIPaths.TASK_HOST, APIPaths.TASK_LIST_PATH,order) if max is None else "{}{}?order={}&max={}".format(APIPaths.TASK_HOST, APIPaths.TASK_LIST_PATH,order,max)
+    uri ="{}{}?order={}".format(APIPaths.TASK_HOST, APIPaths.TASK_LIST_PATH, order) if max is None else "{}{}?order={}&max={}".format(APIPaths.TASK_HOST, APIPaths.TASK_LIST_PATH, order, max)
 
     response: Response = requests.get(uri, headers={'Content-Type': 'application/json', "Authorization": "{}".format(request.headers.get('Authorization'))})
-
-    # TODO CHECK STATUS CODE FOR ERRORS
-    # response_body = json.loads(response.text)
 
     if response.status_code != 200:
         return {
@@ -41,23 +35,21 @@ def create_task():
 
     response: Response = requests.post(uri, data=data, files=files, headers={"Authorization": "{}".format(request.headers.get('Authorization'))})
 
-    # response_body = json.loads(response.text)
     if response.status_code != 201:
         return {
             "code": "fail"
         }, response.status_code
 
-    return {}
+    return json.loads(response.text)
 
 
 @task_routes.route('/<id_task>', methods=['GET'])
 @jwt_required()
 def get_task(id_task: str):
-
     uri = "{}{}{}".format(APIPaths.TASK_HOST, APIPaths.TASK_SINGLE_QUERY_PATH, id_task)
 
     response: Response = requests.get(uri, headers={"Authorization": "{}".format(request.headers.get('Authorization'))})
-    # response_body = json.loads(response.text)
+
     if response.status_code != 200:
         return {
             "code": "fail"
@@ -71,7 +63,7 @@ def get_task(id_task: str):
 def delete_task(id_task: str):
     uri = "{}{}{}".format(APIPaths.TASK_HOST, APIPaths.TASK_SINGLE_QUERY_PATH, id_task)
     response: Response = requests.delete(uri, headers={"Authorization": "{}".format(request.headers.get('Authorization'))})
-    #response_body = json.loads(response.text)
+
     if response.status_code != 204:
         return {
             "code": "fail"
