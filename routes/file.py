e240b10457d4c6c3148f9a0e350721552646e4b5
Luis <luisfernandosantaospina@gmail.com>
2023-04-16 12:33:27 -0500

    Add file route

--- routes/file.py
+++ routes/file.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from flask import Blueprint, Response, request
-from flask_jwt_extended import jwt_required, get_jwt_identity
+from flask_jwt_extended import jwt_required
 from config import APIPaths
 
 file_routes = Blueprint('file', __name__)
@@ -10,14 +10,13 @@ file_routes = Blueprint('file', __name__)
 @file_routes.route('/<int:id_file>', methods=['GET'])
 @jwt_required()
 def get_tasks(id_file: int):
+    uri = "{}{}{}".format(APIPaths.TASK_HOST, APIPaths.TASK_SINGLE_QUERY_PATH, id_file)
+    response: Response = requests.get(uri, headers={"Authorization": "{}".format(request.headers.get('Authorization'))})
 
-    uri = APIPaths.FILE_HOST + APIPaths.FILE_QUERY_PATH + str(id_file)
-    response: Response = requests.get(uri, headers={})
     response_body = json.loads(response.text)
     if response.status_code != 200:
         return {
             "code": "fail",
-            "msg": response_body["error"]
         }, response.status_code
 
     return {}
